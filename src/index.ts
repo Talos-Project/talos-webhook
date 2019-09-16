820449e65fc9877c054c1ce99aa79dd74a2dd250
Nijat <n.e.mahmudov@asan.gov.az>
2019-09-16 19:07:15 +0400

    fix: Ignore merge approvemnet from MR owner

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as express from 'express'
 import * as bodyparser from 'body-parser'
-import { Gitlab, ProjectId } from 'gitlab'
+import { Gitlab, ProjectId, Branches } from 'gitlab'
 
 require("dotenv").config();
 
@@ -11,17 +11,27 @@ const gitlabApi = new Gitlab({
 
 // gitlabApi.Users.current().then(val => console.log(val))
 // gitlabApi.Users.all().then(_ => console.log(_))
+// gitlabApi.Pipelines.show(528, 3721).then(_ => console.log(_))
+// gitlabApi.MergeRequests.all({projectId: 528, state: "opened", source_branch: "feature-x"}).then(_ => console.log(_))
 
 const PORT = process.env.NODE_PORT || 3000;
 const app = express();
 const api = express.Router();
 
 api.post("/hook", (req, res) => {
-  const { project_id, object_kind, object_attributes, builds, merge_request } = req.body;
+  const { project_id, object_kind, object_attributes, builds, merge_request, ref } = req.body;
   console.log(req.body);
 
-  if (object_kind === "note") {
-    handleNoteEvent(object_attributes, project_id, merge_request);
+  switch (object_kind) {
+    case "note":
+      handleNoteEvent(object_attributes, project_id, merge_request);
+      break;
+    case "build":
+      handleBuildEvent(project_id, ref);
+      console.log(req.body)
+    default:
+      // console.log(object_kind)
+      break;
   }
 
   res.json({ status: "success" });
@@ -38,7 +48,7 @@ function handleNoteEvent(object_attributes: any, project_id: ProjectId, merge_re
   const { note } = object_attributes;
   if (note.includes('/retest') || note.includes('/ready-for-test'))
     gitlabApi.Pipelines.create(project_id, merge_request.source_branch);
-  if (note.includes('/approve') && merge_request.assignee_id === object_attributes.author_id)
+  if (note.includes('/approve') && merge_request.assignee_id === object_attributes.author_id && object_attributes.author_id !== merge_request.author_id)
     gitlabApi.MergeRequests.accept(project_id, merge_request.iid);
   if (note.includes('/meow')) {
     gitlabApi.MergeRequestNotes.create(project_id, merge_request.iid, "![cat](https://cataas.com/cat)");
@@ -61,4 +71,9 @@ function handleNoteEvent(object_attributes: any, project_id: ProjectId, merge_re
       gitlabApi.MergeRequestNotes.create(project_id, merge_request.iid, `Download code quality [report](${reportUrl})`);
     })
   }
+}
+
+function handleBuildEvent(projectId: ProjectId, ref: string) {
+  // TODO: Implement report attaching after job finishes successful 
+  gitlabApi.MergeRequests.all({projectId, state: "opened", ref}).then(_ => console.log(_))
 }
