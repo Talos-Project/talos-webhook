3dbae507bafb9326804cd989d7b2dfc0557191ee
Nijat <n.e.mahmudov@asan.gov.az>
2019-09-24 17:45:41 +0400

    fix: grettings

--- src/index.ts
+++ src/index.ts
@@ -89,12 +89,17 @@ async function handleNoteEvent(noteEvt: NoteEvent) {
   }
   if (note.includes('/ready-for-review')) {
     const { approvers, reviewers } = await getCollaborators(noteEvt.project_id)
-    const assignee_id = await gitlabApi.Users.all()
+    const potentialAssignees = await gitlabApi.Users.all()
       .then(_ => (<User[]>_)
-      .filter(u => approvers.includes(u.username)).map(u => u.id).find(uid => uid !== noteEvt.merge_request.author_id))
-    console.log(assignee_id)
+      .filter(u => approvers.includes(u.username)))
+    const assignee_id = potentialAssignees.map(u => u.id).find(uid => uid !== noteEvt.merge_request.author_id)
+    const assigneeName = potentialAssignees.find(a => a.id === assignee_id).username
     gitlabApi.MergeRequests.edit(noteEvt.project_id, noteEvt.merge_request.iid, { assignee_id })
-    reply(noteEvt.project_id, noteEvt.merge_request.iid, "/cc @" + reviewers.join(" @"))
+    const messageForReviewers = `Grettings @${reviewers.join(", @")}
+    ! You have been selected for review. You can trigger tests using \`/test\` command. 
+    A detailed report will be sumbitted on this thread. 
+    Once you are satisfied with MR and test results type \`/lgtm\` to signal your positive feedback to approver (@${assigneeName}).`;
+    reply(noteEvt.project_id, noteEvt.merge_request.iid, messageForReviewers)
   }
   if (note.includes('/meow')) {
     // Use https://api.thecatapi.com/v1/images/search?format=json&results_per_page=1 for better caturday api
