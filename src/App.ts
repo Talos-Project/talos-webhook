64ffb96b69d8590aa57e6b2fd2aad7ae81d14272
Nijat <n.e.mahmudov@asan.gov.az>
2019-10-01 11:29:38 +0400

    refactor: Extract core functionality into its module

--- src/App.ts
+++ src/App.ts
@@ -1,85 +1,36 @@
-import * as express from 'express'
-import * as bodyparser from 'body-parser'
-import { Gitlab, ProjectId } from 'gitlab'
+import { ProjectId } from 'gitlab';
 import { MergeRequestEvent } from './MergeRequestEvent';
 import { User } from './User';
 import { NoteEvent } from './NoteEvent';
-import * as YAML from 'yaml'
 import { RepoBlob } from './RepoBlob';
 import { Collaborators } from './Collaborators';
 import { MergeRequest } from './MergeRequest';
 import { GitlabReviewers } from './GitlabReviewers';
+import { botInfo, gitlabApi, ownersFileName } from './Entrypoint';
+import * as YAML from 'yaml'
 
-require("dotenv").config();
-
-const ownersFileName = 'OWNERS';
-const gitlabApi = new Gitlab({
-  host: process.env.host,
-  token: process.env.token
-});
-
-let botInfo: User;
-
-gitlabApi.Users.current().then(u => botInfo = <User>u)
-
-const PORT = process.env.NODE_PORT || 3000;
-const app = express();
-const api = express.Router();
-
-api.post("/hook", (req, res) => {
-  const { object_kind } = req.body;
-  console.log(req.body);
-
-  switch (object_kind) {
-    case "note":
-      handleNoteEvent(req.body);
-      break;
-    case "pipeline":
-      handlePipelineEvent(req.body);
-      break;
-    case "merge_request":
-      handleWelcomeEvent(req.body);
-      break;
-    default:
-      console.log("Unhandled request")
-      break;
-  }
-
-  res.json({ status: "success" });
-});
-
-app.use(bodyparser.json());
-app.use("/api/v1", api);
-
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
-
-async function handleNoteEvent(noteEvt: NoteEvent) {
+export async function handleNoteEvent(noteEvt: NoteEvent) {
   const { note } = noteEvt.object_attributes;
-
   // Prevent bot's command execution
   if (noteEvt.user.username === botInfo.username) {
-    return
+    return;
   }
-
   if (note.includes('/test')) {
-    const { approvers, reviewers } = await getCollaborators(noteEvt.project_id)
+    const { approvers, reviewers } = await getCollaborators(noteEvt.project_id);
     if (!(approvers.includes(noteEvt.user.username) || reviewers.includes(noteEvt.user.username)))
       return;
     handleTestEvent(noteEvt.merge_request, noteEvt.project_id);
-    reply(noteEvt.project_id, noteEvt.merge_request.iid,
-      `@${noteEvt.user.username}, your requst for tests has been submitted! I will post test results once they are ready.`)
+    reply(noteEvt.project_id, noteEvt.merge_request.iid, `@${noteEvt.user.username}, your requst for tests has been submitted! I will post test results once they are ready.`);
   }
   if (note.includes('/lgtm')) {
-    const { approvers, reviewers } = await getCollaborators(noteEvt.project_id)
+    const { approvers, reviewers } = await getCollaborators(noteEvt.project_id);
     if (!(approvers.includes(noteEvt.user.username) || reviewers.includes(noteEvt.user.username)))
       return;
-    const labels = await gitlabApi.MergeRequests.show(noteEvt.project_id, noteEvt.merge_request.iid).then(mr => (<MergeRequest>mr).labels)
-    gitlabApi.MergeRequests.edit(noteEvt.project_id, noteEvt.merge_request.iid, { labels: labels.join(",").concat(",lgtm") })
+    const labels = await gitlabApi.MergeRequests.show(noteEvt.project_id, noteEvt.merge_request.iid).then(mr => (<MergeRequest>mr).labels);
+    gitlabApi.MergeRequests.edit(noteEvt.project_id, noteEvt.merge_request.iid, { labels: labels.join(",").concat(",lgtm") });
   }
   if (note.includes('/approve')) {
-    const users = await gitlabApi.Users.all().then(_ => (<User[]>_))
+    const users = await gitlabApi.Users.all().then(_ => (<User[]>_));
     const userId = users.find(u => u.username === noteEvt.user.username).id;
     if (userId === noteEvt.merge_request.assignee_id)
       // TODO Update weight maps once MR is closed
@@ -89,44 +40,34 @@ async function handleNoteEvent(noteEvt: NoteEvent) {
         .catch(e => console.log(e));
   }
   if (note.includes('/ready-for-review')) {
-    handleReadyForReviewEvent(noteEvt)
+    handleReadyForReviewEvent(noteEvt);
   }
   if (note.includes('/meow')) {
     // Use https://api.thecatapi.com/v1/images/search?format=json&results_per_page=1 for better caturday api
     gitlabApi.MergeRequestNotes.create(noteEvt.project_id, noteEvt.merge_request.iid, "![cat](https://cataas.com/cat)");
   }
 }
-
 async function handleReadyForReviewEvent(evt: NoteEvent) {
   // TODO Move Blunderbuss selection to a function
   // and invoke that function if only assignee is not assigned
-  const gReviewers = new GitlabReviewers(gitlabApi)
-  const owners = await getCollaborators(evt.project_id)
-  const users = await gitlabApi.Users.all().then(_ => (<User[]>_))
-  const author = users.find(u => evt.merge_request.author_id === u.id).username
-
+  const gReviewers = new GitlabReviewers(gitlabApi);
+  const owners = await getCollaborators(evt.project_id);
+  const users = await gitlabApi.Users.all().then(_ => (<User[]>_));
+  const author = users.find(u => evt.merge_request.author_id === u.id).username;
   const potentialApprovers = users.filter(u => owners.approvers.includes(u.username))
-    .filter(u => u.id !== evt.merge_request.author_id).map(r => r.username)
-
-  const assignee = users.find(u => u.username === potentialApprovers[Math.floor(Math.random() * potentialApprovers.length)])
-
+    .filter(u => u.id !== evt.merge_request.author_id).map(r => r.username);
+  const assignee = users.find(u => u.username === potentialApprovers[Math.floor(Math.random() * potentialApprovers.length)]);
   const potentialReviewers = users.filter(u => owners.reviewers.includes(u.username))
-    .filter(u => u.id !== evt.merge_request.author_id).map(r => r.username)
-
-  const ReviewersWeigthMaps = await gReviewers.getAll()
-
+    .filter(u => u.id !== evt.merge_request.author_id).map(r => r.username);
+  const ReviewersWeigthMaps = await gReviewers.getAll();
   const zeroWeightRevs = potentialReviewers
     .filter(r => !ReviewersWeigthMaps.map(wm => wm.name).includes(r));
-
   // Updating current state of reviewers weightmap with new users from owners file
-  zeroWeightRevs.forEach(r => gReviewers.update({ name: r, weight: 0 }))
-
+  zeroWeightRevs.forEach(r => gReviewers.update({ name: r, weight: 0 }));
   const reviewers = (await gReviewers.getAll())
     .filter(wm => potentialReviewers.includes(wm.name))
-    .sort((a, b) => a.weight - b.weight).map(r => r.name).slice(0, 2)
-
-  gitlabApi.MergeRequests.edit(evt.project_id, evt.merge_request.iid, { assignee_id: assignee.id })
-
+    .sort((a, b) => a.weight - b.weight).map(r => r.name).slice(0, 2);
+  gitlabApi.MergeRequests.edit(evt.project_id, evt.merge_request.iid, { assignee_id: assignee.id });
   const messageForReviewers = [
     "The following table represents the participants of this MR",
     "", "Name | Role", "---|---",
@@ -134,17 +75,14 @@ async function handleReadyForReviewEvent(evt: NoteEvent) {
     `@${assignee.username} | Approver`,
     ...reviewers.map(r => `@${r} | Reviewer`), "",
     "Reviewers can accept the MR using `/lgtm` command. Approver can merge the MR using `/approve`."
-  ]
-
+  ];
   // TODO Update weight maps once reviewers are assigned
   gitlabApi.MergeRequests.show(evt.project_id, evt.merge_request.iid).then(mr => {
     const changes_count = parseInt((<MergeRequest>mr).changes_count);
-    reviewers.forEach(r => gReviewers.increaseWeight({name:r, weight: changes_count}))
-  })
-
-  reply(evt.project_id, evt.merge_request.iid, messageForReviewers.join('\n'))
+    reviewers.forEach(r => gReviewers.increaseWeight({ name: r, weight: changes_count }));
+  });
+  reply(evt.project_id, evt.merge_request.iid, messageForReviewers.join('\n'));
 }
-
 function handleTestEvent(merge_request: any, project_id: ProjectId) {
   const variables = [
     { "key": "MR_ID", "value": merge_request.iid.toString() },
@@ -154,21 +92,18 @@ function handleTestEvent(merge_request: any, project_id: ProjectId) {
     variables
   }).catch(err => console.log(err));
 }
-
-function handlePipelineEvent(pipeline) {
-  const projectId = pipeline.project.id
-  const status = pipeline.object_attributes.detailed_status
-  const MR_ID = parseInt(pipeline.object_attributes.variables.find(v => v.key === "MR_ID").value)
+export function handlePipelineEvent(pipeline) {
+  const projectId = pipeline.project.id;
+  const status = pipeline.object_attributes.detailed_status;
+  const MR_ID = parseInt(pipeline.object_attributes.variables.find(v => v.key === "MR_ID").value);
   const jobID = pipeline.builds.find(b => b.name === "Code Quality").id;
   const reportUrl = generateReportURL(pipeline.project.web_url, jobID);
   if (status === "passed")
-    reply(projectId, MR_ID, generateReportSummary(reportUrl))
+    reply(projectId, MR_ID, generateReportSummary(reportUrl));
 }
-
 function generateReportURL(projectURL: string, jobID: number | string) {
   return projectURL + '/-/jobs/' + jobID + "/artifacts/raw/gl-code-quality-report.html?inline=false";
 }
-
 function generateReportSummary(reportURL: string) {
   return `
 The following table represents several test results, say \`/test\` to start them over: 
@@ -178,9 +113,8 @@ Test Name | Status | Details
 Code Quality | :white_check_mark: | [Link](${reportURL}) 
 Unit Tests | :warning: | N/A 
 Code Coverage | :no_entry: | Link 
-  `
+  `;
 }
-
 // TODO: Extract requestReport to an interface so that you can have diffirent types of reports (code quality, coverage, etc.) 
 // TODO: Get all artifacts from jobs with stage test 
 // TODO: Unzip json reports from artifacts and append them to merge request discussion 
@@ -188,26 +122,22 @@ Code Coverage | :no_entry: | Link
 // TODO: Let users choose the type (code quality) and format (html,json) of report 
 // TODO: Expose report name as function argument 
 // TODO: Extract artifact path from pipeline variable 
-
-function handleWelcomeEvent(mr: MergeRequestEvent) {
+export function handleWelcomeEvent(mr: MergeRequestEvent) {
   if (mr.object_attributes.action === "open")
-    reply(mr.project.id, mr.object_attributes.iid, generateWelcomeMessage(mr.user))
+    reply(mr.project.id, mr.object_attributes.iid, generateWelcomeMessage(mr.user));
 }
-
 function generateWelcomeMessage(user: User) {
   return `
     Hi @${user.username}! Thanks for your MR.
     Once your changes are ready to be merged type \`/ready-for-review\` so that I can assign reviewers.
-  `
+  `;
 }
-
 function getCollaborators(project_id: number | string): Promise<Collaborators> {
   return gitlabApi.RepositoryFiles.show(project_id, ownersFileName, 'master')
     .then(_ => {
-      return YAML.parse(Buffer.from((<RepoBlob>_).content, "base64").toString("ascii"))
-    })
+      return YAML.parse(Buffer.from((<RepoBlob>_).content, "base64").toString("ascii"));
+    });
 }
-
 function reply(projectId: ProjectId, mrId: any, text: string) {
-  gitlabApi.MergeRequestNotes.create(projectId, mrId, text).catch(err => console.log(err))
+  gitlabApi.MergeRequestNotes.create(projectId, mrId, text).catch(err => console.log(err));
 }
---
+++ src/Entrypoint.ts
@@ -0,0 +1,51 @@
+import * as express from 'express'
+import * as bodyparser from 'body-parser'
+import { Gitlab } from 'gitlab'
+import { User } from './User';
+import { handleNoteEvent, handlePipelineEvent, handleWelcomeEvent } from './App';
+
+require("dotenv").config();
+
+export const ownersFileName = 'OWNERS';
+export const gitlabApi = new Gitlab({
+  host: process.env.host,
+  token: process.env.token
+});
+
+export let botInfo: User;
+
+gitlabApi.Users.current().then(u => botInfo = <User>u)
+
+const PORT = process.env.NODE_PORT || 3000;
+const app = express();
+const api = express.Router();
+
+api.post("/hook", (req, res) => {
+  const { object_kind } = req.body;
+  console.log(req.body);
+
+  switch (object_kind) {
+    case "note":
+      handleNoteEvent(req.body);
+      break;
+    case "pipeline":
+      handlePipelineEvent(req.body);
+      break;
+    case "merge_request":
+      handleWelcomeEvent(req.body);
+      break;
+    default:
+      console.log("Unhandled request")
+      break;
+  }
+
+  res.json({ status: "success" });
+});
+
+app.use(bodyparser.json());
+app.use("/api/v1", api);
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
+
