9662b82be6631747d86d719c072ff822cdabcfbd
Nijat <n.e.mahmudov@asan.gov.az>
2019-10-07 17:59:49 +0400

    refactor: Add demuxer

---
+++ src/Demuxer.ts
@@ -0,0 +1,17 @@
+import { Plugin } from "./Plugin"
+
+export class Demuxer {
+
+    plugins: Plugin<any, Promise<any>>[]
+
+    constructor(plugins: Plugin<any, Promise<any>>[]) {
+        this.plugins = plugins
+    }
+
+    dispatch(payload: any) {
+        this.plugins.forEach(plugin => {
+            plugin.handle(payload)
+        })
+    }
+
+}
