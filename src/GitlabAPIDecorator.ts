5af66ca974a576ec8e6db77769148af9f93bdca3
Nijat <n.e.mahmudov@asan.gov.az>
2019-10-08 11:06:49 +0400

    fixup! chore: define MergeRequestNotes api

--- src/GitlabAPIDecorator.ts
+++ src/GitlabAPIDecorator.ts
@@ -4,7 +4,7 @@ import { GitProvider } from './GitProvider';
 import { MergeRequestDecorator } from './MergeRequestDecorator';
 
 export class GitlabAPIDecorator implements GitProvider {
-
+    MergeRequestNotes: import("./GitProvider").MergeRequestNotes;
     MergeRequests: import("./GitProvider").MergeRequests;
     RepositoryFiles: import("./GitProvider").RepositoryFiles;
     Snippets: import("./GitProvider").Snippets;
@@ -16,5 +16,8 @@ export class GitlabAPIDecorator implements GitProvider {
         this.provider = new Gitlab(config)
         this.Users = new GitlabUsersDecorator(this.provider.Users,this.provider.Snippets)
         this.MergeRequests = new MergeRequestDecorator(this.provider.MergeRequests, this.provider.Snippets, this.Users)
+        this.MergeRequestNotes = this.provider.MergeRequestNotes
+        this.Snippets = this.provider.Snippets
+        this.RepositoryFiles = this.provider.RepositoryFiles
     }
 }
