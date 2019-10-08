32e8a8603131d57d7de101115fcdefac7c2c6af7
Nijat <n.e.mahmudov@asan.gov.az>
2019-10-08 11:20:38 +0400

    refactor: pass payload to demuxer

--- src/Entrypoint.ts
+++ src/Entrypoint.ts
@@ -2,7 +2,12 @@ import * as express from 'express'
 import * as bodyparser from 'body-parser'
 import { Gitlab } from 'gitlab'
 import { User } from './User';
-import { handleNoteEvent, handlePipelineEvent, handleMergeRequestEvent } from './App';
+import { Demuxer } from './Demuxer';
+import { GitlabAPIDecorator } from './GitlabAPIDecorator';
+import { PluginFactory } from './PluginFactory';
+import { GenericEvent } from './GenericEvent';
+
+const plugins = ["meow", "lgtm"]
 
 require("dotenv").config();
 export const ownersFileName = 'OWNERS';
@@ -12,36 +17,37 @@ const gitConfig = {
   token: process.env.token
 };
 
-export const gitlabApi = new Gitlab(gitConfig);
+const gitExt = new GitlabAPIDecorator(gitConfig)
+
+const factory = new PluginFactory(gitExt)
+const dmuxer = new Demuxer(plugins.map(plugin => factory.make(plugin)))
+
 
 export let botInfo: User;
 
-gitlabApi.Users.current().then(u => botInfo = <User>u)
+gitExt.Users.current().then(u => botInfo = <User>u)
 
 const PORT = process.env.NODE_PORT || 3000;
 const app = express();
 const api = express.Router();
 
 api.post("/hook", (req, res) => {
-  const { object_kind } = req.body;
-  console.log(req.body);
 
-  switch (object_kind) {
-    case "note":
-      handleNoteEvent(req.body);
-      break;
-    case "pipeline":
-      handlePipelineEvent(req.body);
-      break;
-    case "merge_request":
-      handleMergeRequestEvent(req.body);
-      break;
-    default:
-      console.log("Unhandled request")
-      break;
+  res.contentType("application/json")
+
+  const payload = <GenericEvent> req.body;
+
+  console.log(payload);
+
+  if (payload.user.username === botInfo.username) {
+    res.json({ status: "ignored", reason: "self-generated payload" })
+    return
   }
 
+  dmuxer.dispatch(payload)
+
   res.json({ status: "success" });
+  
 });
 
 app.use(bodyparser.json());
---
+++ src/GenericEvent.ts
@@ -0,0 +1,14 @@
+import { ProjectId } from "./Project";
+import { User } from "./User";
+
+export interface GenericEvent {
+    event_type: EventType
+    object_kind: ObjectKind
+    object_attributes: object
+    project_id: ProjectId
+    user: User
+}
+
+// FIXME define all event types
+export type EventType = string
+export type ObjectKind = string
