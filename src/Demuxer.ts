d3f36d001a7e08258b594a2038f9f7b117ef7309
Nijat <n.e.mahmudov@asan.gov.az>
2019-10-16 11:24:09 +0400

    fix: Better logging

--- src/Demuxer.ts
+++ src/Demuxer.ts
@@ -1,23 +1,30 @@
-import { Plugin } from "./interfaces/Plugin"
+import { Plugin } from "./interfaces/Plugin";
 import { Logger } from "./utils/Logger";
 
 export class Demuxer {
-
-    plugins: Plugin<any, Promise<any>>[]
-    logger: Logger 
+    plugins: Plugin<any, Promise<any>>[];
+    logger: Logger;
 
     constructor(plugins: Plugin<any, Promise<any>>[], logger?: Logger) {
-        if (logger)
-            this.logger = logger
+        if (logger) this.logger = logger;
 
-        this.plugins = plugins
+        this.plugins = plugins;
     }
 
     dispatch(payload: any) {
         this.plugins.forEach(plugin => {
-            plugin.handle(payload)
-                .catch(this.logger.error)
-        })
+            plugin
+                .handle(payload)
+                .catch(e =>
+                    this.logger.error(
+                        JSON.stringify(Object.assign(e,
+                            {
+                                pluginName: plugin.constructor.name,
+                                request: payload,
+                            }
+                        ))
+                    )
+                );
+        });
     }
-
 }
--- src/Entrypoint.ts
+++ src/Entrypoint.ts
@@ -1,11 +1,11 @@
-import * as express from 'express'
-import * as bodyparser from 'body-parser'
-import { User } from './interfaces/structs/User';
-import { Demuxer } from './Demuxer';
-import { GitlabClient } from './gitlab/GitlabClient';
-import { PluginFactory } from './PluginFactory';
-import { GenericEvent } from './interfaces/events/GenericEvent';
-import { ConsoleLogger } from './utils/ConsoleLogger';
+import * as express from "express";
+import * as bodyparser from "body-parser";
+import { User } from "./interfaces/structs/User";
+import { Demuxer } from "./Demuxer";
+import { GitlabClient } from "./gitlab/GitlabClient";
+import { PluginFactory } from "./PluginFactory";
+import { GenericEvent } from "./interfaces/events/GenericEvent";
+import { ConsoleLogger } from "./utils/ConsoleLogger";
 
 const plugins = [
   "Caturday",
@@ -14,7 +14,7 @@ const plugins = [
   "TestRunner",
   "Blunderbuss",
   "Approve"
-]
+];
 
 require("dotenv").config();
 
@@ -25,43 +25,49 @@ const gitConfig = {
 
 const gitExt = new GitlabClient(gitConfig);
 
-const factory = new PluginFactory(gitExt)
-const dmuxer = new Demuxer(
-  plugins.map(plugin => factory.make(plugin)),
-  new ConsoleLogger()
-)
+const factory = new PluginFactory(gitExt);
+const logger = new ConsoleLogger();
+const dmuxer = new Demuxer(plugins.map(plugin => factory.make(plugin)), logger);
 
 let botInfo: User;
 
 gitExt.Users.current()
-  .then(u => botInfo = <User>u)
-  .catch(console.error)
+  .then(u => (botInfo = <User>u))
+  .catch(console.error);
+
+const LOG_LEVELS = {
+  INFO: 1,
+  ERROR: 2,
+};
 
 const PORT = process.env.NODE_PORT || 3000;
+const LOG_LEVEL = parseInt(process.env.NODE_LOG_LEVEL) || LOG_LEVELS.ERROR;
+
 const app = express();
 const api = express.Router();
 
 api.post("/hook", (req, res) => {
-
-  res.contentType("application/json")
+  res.contentType("application/json");
 
   const payload = <GenericEvent>req.body;
 
-  console.log(payload);
+  if (LOG_LEVEL === LOG_LEVELS.INFO)
+    logger.info(payload)
 
-  if (payload.user.username === botInfo.username &&
-    payload.object_kind === "note") {
+  if (
+    payload.user.username === botInfo.username &&
+    payload.object_kind === "note"
+  ) {
     res.json({
       status: "ignored",
       reason: "self-generated payload"
-    })
-    return
+    });
+    return;
   }
 
-  dmuxer.dispatch(payload)
+  dmuxer.dispatch(payload);
 
   res.json({ status: "success" });
-
 });
 
 app.use(bodyparser.json());
